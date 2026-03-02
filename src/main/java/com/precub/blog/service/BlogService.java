a44359d8bc2264e56d0286bc986076d0b5b52eef
Adrian <adi.precub@gmail.com>
2026-03-02 09:01:50 +0100

    refresh 2 times/day

--- src/main/java/com/precub/blog/service/BlogService.java
+++ src/main/java/com/precub/blog/service/BlogService.java
@@ -29,7 +29,7 @@ public class BlogService {
     
     // Cache timestamps
     private volatile LocalDateTime lastCacheUpdate = null;
-    private static final long CACHE_DURATION_HOURS = 25;
+    private static final long CACHE_DURATION_HOURS = 12;
 
     public BlogService(IButterCMSClient butterCMSClient) {
         this.butterCMSClient = butterCMSClient;
